b5045e70e0f5019e6b6b67ecc3630e7d18373d52
Isac Kaik <isackaik1@gmail.com>
2024-03-10 14:53:48 -0300

    feature: Iniciado projeto backend

---
+++ dev/backend/portallicitacao/src/main/java/com/ufs/es2/portallicitacao/portallicitacao/PortallicitacaoApplication.java
@@ -0,0 +1,13 @@
+package com.ufs.es2.portallicitacao.portallicitacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortallicitacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortallicitacaoApplication.class, args);
+	}
+
+}
---
+++ dev/backend/portallicitacao/src/test/java/com/ufs/es2/portallicitacao/portallicitacao/PortallicitacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufs.es2.portallicitacao.portallicitacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortallicitacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
