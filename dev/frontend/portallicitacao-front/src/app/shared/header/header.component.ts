b28b803840c81cf36d76441cadd90b87703b1b4c
Joao Pedro Bitencourt <j0310pedro@gmail.com>
2024-03-24 18:57:01 -0300

    fix(frontend) hover removido do header

--- dev/frontend/portallicitacao-front/src/app/shared/header/header.component.ts
+++ dev/frontend/portallicitacao-front/src/app/shared/header/header.component.ts
@@ -9,16 +9,10 @@ import { NavigationEnd, Router } from '@angular/router';
 export class HeaderComponent implements OnInit {
 
   isMenuHovered: boolean;
-  isUnidadesGestorasHovered: boolean;
-  isModalidadesHovered: boolean;
-  isEstadosHovered: boolean;
   rotaAtual: string;
 
   constructor(private router: Router) {
     this.isMenuHovered = false;
-    this.isUnidadesGestorasHovered = false;
-    this.isModalidadesHovered = false;
-    this.isEstadosHovered = false;
     this.rotaAtual = "";
   }
   ngOnInit(): void {
@@ -32,20 +26,6 @@ export class HeaderComponent implements OnInit {
   toggler(event: PointerEvent) {
     if (event.type === "pointerenter") this.isMenuHovered = true;
     if (event.type === "pointerleave") this.isMenuHovered = false;
-  }
-
-  hover(event: MouseEvent, element: string) {
-    if (event.type === "mouseenter") {
-
-      if (element === 'unidadesGestoras') this.isUnidadesGestorasHovered = true;
-      if (element === 'modalidades') this.isModalidadesHovered = true;
-      if (element === 'estados') this.isEstadosHovered = true;
-    }
-    if (event.type === "mouseleave") {
-      if (element === 'unidadesGestoras') this.isUnidadesGestorasHovered = false;
-      if (element === 'modalidades') this.isModalidadesHovered = false;
-      if (element === 'estados') this.isEstadosHovered = false;
-    }
   }
 
 }
