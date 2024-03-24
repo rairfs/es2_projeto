017e96f8fdda4dc3060a12f5ed209c11fdd12b7f
Joao Pedro Bitencourt <j0310pedro@gmail.com>
2024-03-24 13:39:26 -0300

    fix(frontend): ajustes no header

--- dev/frontend/portallicitacao-front/src/app/shared/header/header.component.ts
+++ dev/frontend/portallicitacao-front/src/app/shared/header/header.component.ts
@@ -8,13 +8,36 @@ import { Component } from '@angular/core';
 export class HeaderComponent {
 
   isMenuHovered: boolean;
+  isUnidadesGestorasHovered: boolean;
+  isModalidadesHovered: boolean;
+  isEstadosHovered: boolean;
 
   constructor() {
     this.isMenuHovered = false;
+    this.isUnidadesGestorasHovered = false;
+    this.isModalidadesHovered = false;
+    this.isEstadosHovered = false;
   }
 
   toggler(event: PointerEvent) {
     if (event.type === "pointerenter") this.isMenuHovered = true;
     if (event.type === "pointerleave") this.isMenuHovered = false;
   }
+
+  hover(event: MouseEvent, element: string) {
+    if (event.type === "mouseenter") {
+      console.log(element)
+      
+      if (element === 'unidadesGestoras') this.isUnidadesGestorasHovered = true;
+      if (element === 'modalidades') this.isModalidadesHovered = true;
+      if (element === 'estados') this.isEstadosHovered = true;
+      console.log(this.isModalidadesHovered)
+    }
+    if (event.type === "mouseleave") {
+      if (element === 'unidadesGestoras') this.isUnidadesGestorasHovered = false;
+      if (element === 'modalidades') this.isModalidadesHovered = false;
+      if (element === 'estados') this.isEstadosHovered = false;
+    }
+  }
+  
 }
